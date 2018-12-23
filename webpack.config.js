7905c64e13273e6844923aa2018d737f093e63c5
clubajax <anm9tr@gmail.com>
2018-12-23 09:12:42 -0600

    uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'index.js',
-      library: '',
-      libraryTarget: 'commonjs'
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js',
+        library: '',
+        libraryTarget: 'commonjs'
     },
     externals: [nodeExternals()],
     plugins: [
@@ -19,13 +20,13 @@ module.exports = {
     module: {
       rules: [
         {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/react'],
-            plugins: ['@babel/plugin-transform-classes', '@babel/plugin-proposal-class-properties']
-          }
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/react'],
+                plugins: ['@babel/plugin-transform-classes', '@babel/plugin-proposal-class-properties']
+            }
         }, {
             test: /\.s?css$/,
             use: [
@@ -39,5 +40,29 @@ module.exports = {
                 }]
         }
       ]
-    }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minChunks: 1
+        },
+        minimize: false,
+        minimizer: [
+            new UglifyJSPlugin({
+                sourceMap: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: { inline: false },
+                    output: {
+                        comments: true,
+                        beautify: false,
+                        preserve_line: false,
+                        semicolons: false,
+                        indent_level: 0,
+                        indent_start: 0
+                    }
+                }
+            })
+        ]
+    },
   };
