deedc58d66062c8fa686c52c3b648ddf03378109
clubajax <anm9tr@gmail.com>
2019-01-01 12:22:00 -0600

    add to index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './styles/main.scss';
 import Dropdown from './Dropdown';
+import Checkbox from './Checkbox';
 import ActionButton from './ActionButton';
 import Popup from './Popup';
 import List from './List';
 import equal from './lib/equal';
 
-export { Dropdown, ActionButton, Popup, List, equal };
+export { Checkbox, Dropdown, ActionButton, Popup, List, equal };
